36e8f93838d68e14731585f2e02b1551a0fe48d6
zhanghaoran <ljtyzhr@163.com>
2017-04-25 17:16:10 +0800

    add profile

--- instagram/views.py
+++ instagram/views.py
@@ -10,7 +10,7 @@ from flask import render_template, redirect
 
 from instagram import app, db
 
-from instagram.models import Image
+from instagram.models import Image, User
 
 
 # 首页的展示内容：首页根据图片展示，每张图片都是由哪个用户发上来的
@@ -27,3 +27,11 @@ def image(image_id):
     if image == None:
         return redirect('/')
     return render_template('pageDetail.html', image = image)
+
+# 个人信息界面
+@app.route('/profile/<int:user_id>/')
+def profile(user_id):
+    user = User.query.get(user_id)
+    if user == None:
+        return redirect('/')
+    return render_template('profile.html', user = user)
